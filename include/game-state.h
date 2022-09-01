60b036b9a421dc1764310a6a0def6d2159846aa3
WillChing01 <53403691+WillChing01@users.noreply.github.com>
2022-09-01 11:17:33 +0100

    refactor: move render to game state method

--- include/game-state.h
+++ include/game-state.h
@@ -17,6 +17,32 @@ class GameState
 
         GameState(double sfac) {_sfac=sfac;}
         virtual void update(double dt,sf::Vector2i mouse_pos)=0;
+        virtual void render(sf::RenderWindow &window)
+        {
+            window.clear(_background);
+
+            for (int i=0;i<_shapes.size();i++)
+            {
+                window.draw(*_shapes[i]);
+            }
+            for (int i=0;i<_buttons.size();i++)
+            {
+                window.draw(_buttons[i]._shape);
+                window.draw(_buttons[i]._text);
+            }
+            for (int i=0;i<_inputboxes.size();i++)
+            {
+                window.draw(_inputboxes[i]._shape);
+                window.draw(_inputboxes[i]._backtext);
+                window.draw(_inputboxes[i]._text);
+                window.draw(_inputboxes[i]._cursor);
+            }
+            for (int i=0;i<_importantShapes.size();i++)
+            {
+                window.draw(*_importantShapes[i]);
+            }
+            window.display();
+        }
 };
 
 #endif // GAME-STATE_H_INCLUDED
